6a1870a011dcca61363f36e59f8723233370c92e
joelmasters <joelmasters@gmail.com>
2025-11-25 20:41:22 -0800

    removed podspec files

--- CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-I haven't shipped a release of SwiftyChrono yet. Stay tuned!
--- Package.swift
+++ Package.swift
@@ -5,5 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyChrono",
-    dependencies: []
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v11),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftyChrono",
+            targets: ["SwiftyChrono"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SwiftyChrono",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftyChronoTests",
+            dependencies: ["SwiftyChrono"],
+            path: "Tests"),
+        
+    ]
+)
 )
